4c0b1dbe846954c6c77fdd96bc28e68938e99b60
joota34256 <fuji_8761@yahoo.co.jp>
2021-05-23 19:42:42 +0900

    データ登録の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,except: [:index]
+
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name,:description,:shipping_charge_id,:prefecture_id,:shipping_date_id,:price,:category_id,:state_id,:image).merge(user_id:current_user.id)
   end
 end
---
+++ app/models/state.rb
@@ -0,0 +1,12 @@
+class State < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '未使用'},
+    { id: 3, name: '未使用に近い'},
+    { id: 4, name: 'やや傷、汚れあり'},
+    { id: 5, name: '傷、汚れあり'},
+    { id: 6, name: '大きな傷、汚れあり'},
+  ]
+  include ActiveHash::Associations
+  has_many :items
+end
