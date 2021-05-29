05abe3882c0fe349f121dd8b9989eadab4b409b2
joota34256 <fuji_8761@yahoo.co.jp>
2021-05-29 12:29:45 +0900

    2つのテーブルにデータ保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,25 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_buyer = PurchaseBuyer.new
   end
+
+  def create
+    @purchase_buyer = PurchaseBuyer.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    binding.pry
+    if @purchase_buyer.valid?
+      @purchase_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :address, :building_number, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
---
+++ app/models/purchase_buyer.rb
@@ -0,0 +1,19 @@
+class PurchaseBuyer
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_number, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, numericality: { only_integer: true } 
+    validates :prefecture_id , numericality: { other_than: 1 } 
+    validates :city
+    validates :address
+    validates :phone_number, numericality: { only_integer: true } 
+    validates :user_id
+    validates :item_id
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Buyer.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building_number: building_number, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
