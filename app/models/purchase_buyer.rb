b2817fef567ffb10714836a9b90dcceb73da5a9c
joota34256 <fuji_8761@yahoo.co.jp>
2021-05-29 21:25:35 +0900

    テストコード実装

--- app/models/purchase_buyer.rb
+++ app/models/purchase_buyer.rb
@@ -3,11 +3,11 @@ class PurchaseBuyer
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building_number, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, numericality: { only_integer: true }, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id , numericality: { other_than: 1 } 
     validates :city
     validates :address
-    validates :phone_number, numericality: { only_integer: true }, length: { in: 11 } } 
+    validates :phone_number, numericality: { only_integer: true,  }, length: { is: 11 }
     validates :user_id
     validates :item_id
     validates :token
