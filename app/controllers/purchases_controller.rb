1562de5164af182e7cd254b29dd483cc360c3e80
joota34256 <fuji_8761@yahoo.co.jp>
2021-05-29 19:48:31 +0900

    アクセス制限

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_root
+  
   def index
-    @item = Item.find(params[:item_id])
     @purchase_buyer = PurchaseBuyer.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_buyer = PurchaseBuyer.new(purchase_params)
     if @purchase_buyer.valid?
       pay_item
@@ -17,6 +19,14 @@ class PurchasesController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_root
+    redirect_to root_path if @item.purchase.present?
+    redirect_to root_path if @item.user_id == current_user.id
+  end
 
   def purchase_params
     params.require(:purchase_buyer).permit(:postal_code, :prefecture_id, :city, :address, :building_number, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
